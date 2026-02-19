cbcf6791b27dabb79ac16efa35193f3bbff59208
Sachin Pikle <sachin.pikle@oracle.com>
2026-02-19 13:12:03 +0530

    Expressions in f strings

--- simple-hello/hello_world.py
+++ simple-hello/hello_world.py
@@ -7,5 +7,12 @@ def main():
     discount_applied = 10
     print(f"{user_id=}, {order_total=}, {discount_applied=}")
 
+    ## Eliminate temporary variables with expressions in f strings
+    print(f"Discount amount: {order_total * discount_applied / 100}")
+    print(f"Username: {user_id.upper()}")
+    limit = 50
+    used = 49
+    print(f"Items remaining: {max(limit - used, 0)}")
+
 if __name__ == "__main__":
     main()
